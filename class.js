ea9275efa627079f6790958f4ce5857e1edf1fbf
Sergio Galdames <s.galdames.f@gmail.com>
2022-08-08 01:59:43 -0400

    Se actualiza DOM, mejora de las funciones principales de la pagina, además del uso de JSON y localStorage

--- class.js
+++ class.js
@@ -4,9 +4,8 @@ const formatoPeso = new Intl.NumberFormat('es-CL', {
     minimumFractionDigits: 0
 })
 
-
 class Vehiculo {
-    constructor(marca, modelo, año, cc, valor, color, rendimiento,descripcion) {
+    constructor(marca, modelo, año, cc, valor, color, rendimiento,descripcion,imagen) {
         this.marca = marca;
         this.modelo = modelo;
         this.año = año;
@@ -14,23 +13,33 @@ class Vehiculo {
         this.valor = valor;
         this.color = color;
         this.rendimiento = rendimiento;
-        this.descripcion=descripcion
+        this.descripcion=descripcion;
+        this.imagen=imagen;
     }
-    
 }
 
-const kawasaki1 = new Vehiculo("KAWASAKI", "NINJA 400", "Año: " + 2022, "Cilindrada: "+398, formatoPeso.format(6300000), "COLOR: NEGRO", 4.1,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.")
-const kawasaki3 = new Vehiculo("KAWASAKI", "NINJA ZX-6R", "Año: " + 2021,"Cilindrada: "+ 598, formatoPeso.format(12000000), "COLOR: NEGRO", 5.5,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.")
-const kawasaki5 = new Vehiculo("KAWASAKI", "NINJA ZX-10R", "Año: " + 2021,"Cilindrada: "+ 998, formatoPeso.format(22000000), "COLOR: NEGRO", 5.9,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.")
-const kawasaki6 = new Vehiculo("KAWASAKI", "NINJA H2-R", "Año: " + 2019,"Cilindrada: "+ 998, formatoPeso.format(36000000), "COLOR: NEGRO Espejo", 8.5,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.")
-const ducati1 = new Vehiculo("DUCATI", "MONSTER 821", "Año: " + 2020,"Cilindrada: "+ 821, formatoPeso.format(9000000), "COLOR: ROJO DUCATI", 5.2,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.")
-const ducati3 = new Vehiculo("DUCATI", "PANIGALE 959", "Año: " + 2020,"Cilindrada: "+ 959, formatoPeso.format(18000000), "COLOR: ROJO DUCATI", 5.6,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.")
-const ducati4 = new Vehiculo("DUCATI", "MODELO: PANIGALE v4 S", "Año: " + 2022,"Cilindrada: "+ 1199, formatoPeso.format(32000000), "COLOR: TRI COLOR ITALIANO", 6.9,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.")
-const ducati5 = new Vehiculo("DUCATI", "MULTISTRADA V2 S", "Año: " + 2022, 1200, formatoPeso.format(20000000), "COLOR: ROJO DUCATI", 5.9,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.")
-const ducati6 = new Vehiculo("DUCATI", "DIAVEL", "Año: " + 2020,"Cilindrada: "+ 1260, formatoPeso.format(30000000), "COLOR: GRIS ARSENICA", 5.4,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.")
+const kawasaki1 = new Vehiculo("KAWASAKI", "NINJA 400", "Año: " + 2022, "Cilindrada: "+398, 6300000, "COLOR: NEGRO", 4.1,"La Ninja® 400 2020 de Kawasaki ofrece la mayor cilindrada de su categoría con 399 cc con la sofisticación de la potencia de dos cilindros. El rendimiento deportivo, la excelente ergonomía y el manejo ligero ofrecen una conducción suave y manejable que atrae a distintos pilotos por su rendimiento deportivo de alta velocidad. Los bajos asientos, el agresivo estilo moderno de la carrocería y los faros LED hacen de la Ninja 400 la elección ideal para los pilotos que buscan entrar en el mundo deportivo.", "https://kawasaki.melhuish.cl/wp-content/uploads/2020/02/400-blanca-lado.jpg")
+const kawasaki3 = new Vehiculo("KAWASAKI", "NINJA ZX-6R", "Año: " + 2021,"Cilindrada: "+ 598, 12000000, "COLOR: NEGRO", 5.5,"La Kawasaki Ninja ZX-6R es la deportiva de altas prestaciones que en encuadra en la categoría supersport. Ofrece un uso dual, carretera/circuito, para los que quieren disfrutar del radical espíritu Ninja con un motor de 636 cc que ofrece una potencia máxima de 130 CV. Hereda mucho de la descatalogada Ninja ZX-6R 636 pero está dotada de la más moderna tecnología y de una apariencia inspirada en la todopoderosa ZX-10R.", "https://www.mundomotero.com/wp-content/uploads/2018/10/Kawasaki-ZX-6R-2019.jpg")
+const kawasaki5 = new Vehiculo("KAWASAKI", "NINJA ZX-10R", "Año: " + 2021,"Cilindrada: "+ 998, 22000000, "COLOR: NEGRO", 5.9,"Un motor de 998 cc, un manejo excepcional y un estilo agresivo se combinan con una sofisticada electrónica de última generación para hacer que la Ninja ZX-10R ABS esté lista para las carreras o la conducción en la calle. Frenos Brembo® de última generación y control de tracción Kawasaki Sport.", "https://www.motofichas.com/images/phocagallery/Kawasaki/ninja-zx-10r-2021/01-kawasaki-zx-10r-2021-estudio-verde.jpg")
+const kawasaki6 = new Vehiculo("KAWASAKI", "NINJA H2-R", "Año: " + 2019,"Cilindrada: "+ 998, 36000000, "COLOR: NEGRO Espejo", 8.5,"El desarrollo de la Ninja H2™R de Kawasaki supera los límites de cualquier otra motocicleta Kawasaki. Al exhibir un potente motor de cuatro cilindros en línea de 998 cc, electrónica de última generación y los últimos frenos Brembo®, la única motocicleta hiperdeportiva sobrealimentada de producción limitada del mundo representa la cúspide de la ingeniería de Kawasaki. Con asombrosa aceleración y una velocidad máxima alucinante solo apta para circuitos, la Ninja H2R ofrece la experiencia más estimulante sobre dos ruedas..", "https://s3.eu-west-1.amazonaws.com/cdn.motorbikemag.es/wp-content/uploads/2018/10/Kawasaki-Ninja-H2R-2019-05-1200x900.jpg")
+/*
+const ducati1 = new Vehiculo("DUCATI", "MONSTER 821", "Año: " + 2020,"Cilindrada: "+ 821, formatoPeso.format(9000000), "COLOR: ROJO DUCATI", 5.2,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.", "https://s3.eu-west-1.amazonaws.com/cdn.motorbikemag.es/wp-content/uploads/2018/10/Kawasaki-Ninja-H2R-2019-05-1200x900.jpg")
+const ducati3 = new Vehiculo("DUCATI", "PANIGALE 959", "Año: " + 2020,"Cilindrada: "+ 959, formatoPeso.format(18000000), "COLOR: ROJO DUCATI", 5.6,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.", "https://s3.eu-west-1.amazonaws.com/cdn.motorbikemag.es/wp-content/uploads/2018/10/Kawasaki-Ninja-H2R-2019-05-1200x900.jpg")
+const ducati4 = new Vehiculo("DUCATI", "MODELO: PANIGALE v4 S", "Año: " + 2022,"Cilindrada: "+ 1199, formatoPeso.format(32000000), "COLOR: TRI COLOR ITALIANO", 6.9,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.", "https://s3.eu-west-1.amazonaws.com/cdn.motorbikemag.es/wp-content/uploads/2018/10/Kawasaki-Ninja-H2R-2019-05-1200x900.jpg")
+const ducati5 = new Vehiculo("DUCATI", "MULTISTRADA V2 S", "Año: " + 2022, 1200, formatoPeso.format(20000000), "COLOR: ROJO DUCATI", 5.9,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.", "https://s3.eu-west-1.amazonaws.com/cdn.motorbikemag.es/wp-content/uploads/2018/10/Kawasaki-Ninja-H2R-2019-05-1200x900.jpg")
+const ducati6 = new Vehiculo("DUCATI", "DIAVEL", "Año: " + 2020,"Cilindrada: "+ 1260, formatoPeso.format(30000000), "COLOR: GRIS ARSENICA", 5.4,"Describir es explicar, de manera detallada y ordenada, cómo son las personas, animales, lugares, objetos, etc. La descripción sirve sobre todo para ambientar la acción y crear una que haga más creíbles los hechos que se narran.", "https://s3.eu-west-1.amazonaws.com/cdn.motorbikemag.es/wp-content/uploads/2018/10/Kawasaki-Ninja-H2R-2019-05-1200x900.jpg")
+const ducatistas = [ducati1, ducati3, ducati4, ducati5, ducati6]
+*/
 
 const kawas = [kawasaki1, kawasaki3, kawasaki5, kawasaki6 ];
+const d = document;
 
-const ducatistas = [ducati1, ducati3, ducati4, ducati5, ducati6]
-const marcas=[kawasaki1, kawasaki3, kawasaki5, kawasaki6,ducati1, ducati3, ducati4, ducati5, ducati6]
+//ELEMENTOS DEL SELECTOR//
+const catalogoKawas = document.getElementById("modelos");
+kawas.forEach((vehiculo) => {
+  const option = document.createElement("option");
+  option.innerText = `${vehiculo.modelo}`;
+  option.value = kawas.indexOf(vehiculo);
+  catalogoKawas.append(option);
 
+});
--- script2.js
+++ script2.js
@@ -1,29 +1,13 @@
- let modelos15= document.getElementById("modelos");
-modelos15.onchange=mostrarSeleccion;
-
-
-//ELEMENTOS DEL SELECTOR//
-const catalogoKawas = document.getElementById("modelos");
-kawas.forEach((vehiculo) => {
-  const option = document.createElement("option");
-  option.innerText = `${vehiculo.modelo}`;
-  option.value = kawas.indexOf(vehiculo);
-  catalogoKawas.append(option);
-
-});
-let htmlCode = ""
-let cotiHTML = ""
-let resultadoCoti = ""
+let precio = document.getElementById
+let modelos15 = document.getElementById("modelos");
+modelos15.onchange = mostrarSeleccion;
 /**
  * MOSTRA SELECCIÓN MOSTRARÁ EL ELEMENTO SELECCIONADO EN EL SELECT Y LO DIBUJARÁ EN EL DOM
  */
 function mostrarSeleccion() {
-
   let combo = document.getElementById("modelos")
   let selected = combo.options[combo.selectedIndex].text
-
   const modeloIngresado = kawas.filter(vehiculo => vehiculo.modelo.includes(selected));
-
   let intento100 = document.getElementById("elementoDinamico")
   intento100.innerHTML = ""
   modeloIngresado.forEach((elemento) => {
@@ -33,40 +17,117 @@ function mostrarSeleccion() {
   <h5>${elemento.marca}</h5>
   <p>${elemento.modelo}   ${elemento.año}</p>
   <p>${elemento.cc} cc</p>
-  <p>${elemento.valor} CLP</p>
+  <p>${formatoPeso.format(elemento.valor)} CLP</p>
+  <img src="${elemento.imagen}"</img>
   <p>${elemento.descripcion}</p>
-  <button id="btnMain" type="submit"class="btn btn-dark">Cotiza con nosotros</button>
-  </div>`
+  <form id="formulario" class="col-lg-6 mx-auto row">
+        <div class="form-group col-lg-12 justify-content-center">
+            <label for="nombre">Ingresa tu nombre</label>
+            <input type="text" class="form-control" id="nombre"  placeholder="Ingresa tu nombre" >
+            <div id="nombreIncorrecto"></div>
+        </div>
+        <div class="form-group col-lg-6 justify-content-center">
+            <label for="monto">Ingrese el monto a solicitar</label>
+            <input type="number" class="form-control" id="monto"  placeholder="Mayor o igual a ${formatoPeso.format((elemento.valor) * 0.2)}" >
+            <div id="montoIncorrecto"></div>
+        </div>
+        <div class="col-lg-6 justify-content-center">
+            <label for="cuotas">Ingrese la cantidad de cuotas</label>
+            <input type="number" class="form-control" id="cuotas" placeholder="Mayor o igual a 6">
+            <div id="cuotaIncorrecto"></div>
+        </div>
+        <button id="btnCoti" type="submit" class="btn btn-dark">Cotiza ahora!</button>
+    </form>`
 
-  
-  let boton = document.getElementById("btnMain")
-  boton.addEventListener("click", cancel);
-  function cancel() {
-    boton.removeEventListener("click", cancel);
-    const cotizacion = document.getElementById("cotizaciones")
-    cotizacion.innerHTML =
+    const formCotizacion = d.getElementById("formulario")
 
-      ` <form class="col-lg-6 mx-auto row">
-            <div class="form-group col-lg-6 justify-content-center">
-                <label for="titularConsulta">Ingrese el el monto a solicitar</label>
-                <input type="text" class="form-control" id="titular" placeholder="Ingrese el el monto a solicitar">
-            </div>
-            <div class="col-lg-6 justify-content-center">
-                <label for="monto">Ingrese la cantidad de cuotas</label>
-                <input type="text" class="form-control" id="monto" placeholder="Ingrese la cantidad de cuotas">
-            </div>
-            <button id="btnCoti" type="submit" class="btn btn-secondary">Cotiza ahora!</button>
-        </form>`
-  }})
-  
-  let botonCoti = document.getElementById("btnCoti")
-  function mostrar() {
-    botonCoti.removeEventListener("click", mostrar);
-    const retultadoCoti = document.getElementById("resultadoCoti")
-    retultadoCoti.innerHTML = `
-  <h2>Hello da</h2>
+    formCotizacion.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const nombre=d.getElementById("nombre").value
+      const monto = d.getElementById("monto").value
+      const cuotas = d.getElementById("cuotas").value
+
+      const datosCotizacion = {
+        nombre,
+        monto,
+        cuotas,
+      }
+      const datosDelCotizante = []
+      if (datosCotizacion.monto > 0 && datosCotizacion.monto <= elemento.valor && datosCotizacion.monto >= (elemento.valor) * 0.2 && datosCotizacion.cuotas > 0 && datosCotizacion.cuotas >= 6) {
+        datosDelCotizante.push(datosCotizacion)
+
+        localStorage.setItem("datosDelCotizante", JSON.stringify(datosDelCotizante))
+        localStorage.setItem("modeloIngresado", JSON.stringify(modeloIngresado))
+
+        const interes = 3 / 100;
+        let mensual = ((interes * datosCotizacion.monto) / (1 - Math.pow(1 + interes, -datosCotizacion.cuotas))).toFixed(0)
+        let pagoTotalPrestamo = (mensual * datosCotizacion.cuotas).toFixed(0)
+        let interesesTotal = (mensual * datosCotizacion.cuotas - datosCotizacion.monto).toFixed(0)
+
+        if (datosCotizacion.monto > 0 && datosCotizacion.monto <= elemento.valor) {
+          const resultadoFinal = document.getElementById("cotizacionFinal")
+          resultadoFinal.innerHTML = `
+<p>El monto solicitado es: <span style="font-weight: 600">${formatoPeso.format(datosCotizacion.monto)}</span></p>
+<p>El pago mensual de las cuotas es de: <span style="font-weight: 600">${formatoPeso.format(mensual)}</span></p>
+<p>El pago total será de: <span style="font-weight: 600">${formatoPeso.format(pagoTotalPrestamo)}</span></p>
+<p>El pago total de intereses es de: <span style="font-weight: 600">${formatoPeso.format(interesesTotal)}</span></p>
 `
-  }
-  botonCoti.addEventListener("click", mostrar);
-  
+
+        } else if (datosCotizacion.monto > elemento.valor) {
+          console.log("El valor del credito es superior al valor del vehiculo")
+        }
+      } else {
+        console.log("no se almacenó")
+      }
+    })
+
+    const montoValido = d.getElementById("monto")
+    const cuotasValidas = d.getElementById("cuotas")
+
+    montoValido.addEventListener("blur", () => {
+      if (montoValido.value > (elemento.valor) * 0.2 && montoValido.value <= elemento.valor) {
+        const error = d.getElementById("montoIncorrecto")
+        error.innerHTML = `<p>Monto solicitado correcto</p>`
+
+      } else if (montoValido.value == 0) {
+        const error = d.getElementById("montoIncorrecto")
+        error.innerHTML = `<p>Monto no puede ser $<span style="font-weight: 600">0</span></p>`
+      } else if (montoValido.value <= (elemento.valor) * 0.2) {
+        const error = d.getElementById("montoIncorrecto")
+        error.innerHTML = `<p>Monto debe ser igual o superior al <span style="font-weight: 600">20%</span> del valor del vehiculo</p>`
+      } else {
+        const error = d.getElementById("montoIncorrecto")
+        error.innerHTML = `<p>El monto no puede ser mayor a <span style="font-weight: 600">${formatoPeso.format(elemento.valor)}</span></p>`
+      }
+    })
+    cuotasValidas.addEventListener("blur", () => {
+      if (cuotasValidas.value > 0 && cuotasValidas.value < 6) {
+        const error = d.getElementById("cuotaIncorrecto")
+        error.innerHTML = `<p>Cuotas deben ser igual o superior a <span style="font-weight: 600">6</span></p>`
+      } else if (cuotasValidas.value == 0) {
+        const error = d.getElementById("cuotaIncorrecto")
+        error.innerHTML = `<p>Las cuotas no pueden ser <span style="font-weight: 600">0</span></p>`
+      } else {
+        const error = d.getElementById("cuotaIncorrecto")
+        error.innerHTML = `<p>Cuotas correctas</p>`
+        true
+      }
+    })
+
+
+  })
+
+}
+
+const datosRegistroS = JSON.parse(localStorage.getItem("datosDelCotizante"))
+const modeloRescatada = JSON.parse(localStorage.getItem("modeloIngresado"))
+console.log(datosRegistroS[0].nombre)
+if (datosRegistroS[0] &&modeloRescatada[0]) {
+  let retomarCompra = document.getElementById("retomar")
+  retomarCompra.innerHTML = `
+  <h3>Hola ${datosRegistroS[0].nombre}! </h3>
+  <h3>¿Ya te decidiste en comprar tu moto?  Estuviste cotizando una <span style="color:red;font-weight: 600">${modeloRescatada[0].modelo}</span></h3>
+  `
+} else {
+console.log("casi")
 }
