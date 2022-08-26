715296bae6411c4b1700f1f046141a7d1e732738
Sergio Galdames <s.galdames.f@gmail.com>
2022-08-26 15:47:17 -0400

    modificaciones sin uso de server

--- funcionesRandom.js
+++ funcionesRandom.js
@@ -8,129 +8,132 @@ const elemento7 = document.getElementById("testimonio7")
 const elemento8 = document.getElementById("testimonio8")
 
 function imagen1() {
-    fetch("/testimonios.json")
+    fetch("https://randomuser.me/api/")
         .then(res => res.json())
         .then(data => {
+
             elemento1.innerHTML = `
+            
             <div class="img-box">
-            <img src="${data[0].imagen}" width="100px" class="img-fluid rounded-circle">
+            <img src="${data.results[0].picture.medium}" width="100px" class="img-fluid rounded-circle">
             </div>
             <div class="thumb-content animate__animated animate__fadeIn">
-            <h4>${data[0].nombre} ${data[0].apellido}</h4>
+            <h4>${data.results[0].name.first} ${data.results[0].name.last}</h4>
             <div>
-            <p>${data[0].testimonio}</p>
+            <p>El ejecutivo me atendió de lujo, me ofreció el mejor credito según mi perfil.</p>
             </div>
         `
         })
 }
 function imagen2() {
-   fetch("/testimonios.json")
+    fetch("https://randomuser.me/api/")
         .then(res => res.json())
         .then(data => {
             elemento2.innerHTML = `
             <div class="img-box">
-            <img src="${data[1].imagen}" width="100px" class="img-fluid rounded-circle">
+            <img src="${data.results[0].picture.medium}" width="100px" class="img-fluid rounded-circle">
             </div>
             <div class="thumb-content animate__animated animate__fadeIn">
-            <h4>${data[1].nombre} ${data[1].apellido}</h4>
+            <h4>${data.results[0].name.first} ${data.results[0].name.last} </h4>
             <div>
-            <p>${data[1].testimonio}</p>
+            <p>Luego de años pude comprar la moto de mis sueños, gracias a SG &#128170</p>
             </div>
         `
         })
 }
 function imagen3() {
-    fetch("/testimonios.json")
+    fetch("https://randomuser.me/api/")
         .then(res => res.json())
         .then(data => {
             elemento3.innerHTML = `
             <div class="img-box">
-            <img src="${data[2].imagen}" width="100px" class="img-fluid rounded-circle">
+            <img src="${data.results[0].picture.medium}" width="100px" class="img-fluid rounded-circle">
             </div>
             <div class="thumb-content animate__animated animate__fadeIn">
-            <h4>${data[2].nombre} ${data[2].apellido}</h4>
+            <h4>${data.results[0].name.first} ${data.results[0].name.last} </h4>
             <div>
-            <p>${data[2].testimonio}</p>
+            <p>He recomendado SG a varies de mis amigues, ahora todes estamos andando en nuestras Ducatis &#128133</p>
             </div>
         `
         })
 }
 function imagen4() {
-    fetch("/testimonios.json")
+    fetch("https://randomuser.me/api/")
         .then(res => res.json())
         .then(data => {
             elemento4.innerHTML = `
             <div class="img-box">
-            <img src="${data[3].imagen}" width="100px" class="img-fluid rounded-circle">
+            <img src="${data.results[0].picture.medium}" width="100px" class="img-fluid rounded-circle">
             </div>
             <div class="thumb-content animate__animated animate__fadeIn">
-            <h4>${data[3].nombre} ${data[3].apellido}</h4>
+            <h4>${data.results[0].name.first} ${data.results[0].name.last}</h4>
             <div>
-            <p>${data[3].testimonio}</p>
+            <p>La atención fue expedita, en menos de 2 semanas ya me habían entregado mi H2R &#9889</p>
             </div>
         `
         })
 }
 function imagen5() {
-    fetch("/testimonios.json")
+    fetch("https://randomuser.me/api/")
         .then(res => res.json())
         .then(data => {
             elemento5.innerHTML = `
             <div class="img-box">
-            <img src="${data[4].imagen}" width="100px" class="img-fluid rounded-circle">
+            <img src="${data.results[0].picture.medium}" width="100px" class="img-fluid rounded-circle">
             </div>
             <div class="thumb-content animate__animated animate__fadeIn">
-            <h4>${data[4].nombre} ${data[4].apellido}</h4>
+            <h4>${data.results[0].name.first} ${data.results[0].name.last}</h4>
+            </div>
             <div>
-            <p>${data[4].testimonio}</p>
+            <p>Al principio tenía mis dudas, pero los ejecutivos me dieron la confianza para comprar mi primera moto &#127949</p>
             </div>
         `
         })
 }
 function imagen6() {
-    fetch("/testimonios.json")
+    fetch("https://randomuser.me/api/")
         .then(res => res.json())
         .then(data => {
             elemento6.innerHTML = `
             <div class="img-box">
-            <img src="${data[5].imagen}" width="100px" class="img-fluid rounded-circle">
+            <img src="${data.results[0].picture.medium}" width="100px" class="img-fluid rounded-circle">
             </div>
             <div class="thumb-content animate__animated animate__fadeIn">
-            <h4>${data[5].nombre} ${data[5].apellido}</h4>
+            <h4>${data.results[0].name.first} ${data.results[0].name.last}</h4>
             <div>
-            <p>${data[5].testimonio}</p>
+            <p>"No hagas una venta, haz un cliente", definitivamente tienen mi total fidelidad, volvería a confiar en SG &#128151</p>
             </div>
         `
         })
 }
 function imagen7() {
-   fetch("/testimonios.json")
+    fetch("https://randomuser.me/api/")
         .then(res => res.json())
         .then(data => {
             elemento7.innerHTML = `
             <div class="img-box">
-            <img src="${data[6].imagen}" width="100px" class="img-fluid rounded-circle">
+            <img src="${data.results[0].picture.medium}" width="100px" class="img-fluid rounded-circle">
             </div>
             <div class="thumb-content animate__animated animate__fadeIn">
-            <h4>${data[6].nombre} ${data[6].apellido}</h4>
+            <h4>${data.results[0].name.first} ${data.results[0].name.last}</h4>
             <div>
-            <p>${data[6].testimonio}</p>
+            <p>El interés de los creditos es realmente bajo, de fácil acceso para mi bolsillo.</p>
             </div>
         `
         })
 }
 function imagen8() {
-    fetch("/testimonios.json")
+    fetch("https://randomuser.me/api/")
         .then(res => res.json())
         .then(data => {
             elemento8.innerHTML = `
             <div class="img-box">
-            <img src="${data[7].imagen}" width="100px" class="img-fluid rounded-circle">
+            <img src="${data.results[0].picture.medium}" width="100px" class="img-fluid rounded-circle">
             </div>
             <div class="thumb-content animate__animated animate__fadeIn">
-            <h4>${data[7].nombre} ${data[7].apellido}</h4>
+            <h4>${data.results[0].name.first} ${data.results[0].name.last}</h4>
             <div>
-            <p>${data[7].testimonio}</p>
+            <p>Excelente servicio &#11088&#11088&#11088</p>
             </div>
         `
         })
@@ -139,28 +142,28 @@ const promesa1 = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(imagen1())
-        }, 1100);
+        }, 1000);
     })
 }
 const promesa2 = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(imagen2())
-        }, 1200);
+        }, 1000);
     })
 }
 const promesa3 = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(imagen3())
-        }, 1300);
+        }, 1000);
     })
 }
 const promesa4 = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(imagen4())
-        }, 1400);
+        }, 1000);
     })
 }
 const promesa5 = () => {
